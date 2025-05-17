b7d3bf1e95ddc276b783d2f16fcc668c8b6f3ee3
elvinlyv <elvinaliyevxeyale@gmail.com>
2025-05-17 08:11:37 +0300

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-const signalingServer = "https://senin-render-urlin.onrender.com"; // Bunu kendi URL’inle değiştir
+const signalingServer = "https://p2p-dosya-transferi.onrender.com"; // Bunu kendi URL’inle değiştir
 const currentUser = localStorage.getItem("username");
 
 let peerConnection;
